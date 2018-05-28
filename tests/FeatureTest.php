c3c1922a23f41aa942115e589ea7bcc4f8f82b98
paramtamtam <slashkot@gmail.com>
2018-05-28 11:24:45 +0500

    One more test (feature) added

---
+++ tests/FeatureTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AvtoDev\EventsLogLaravel\Tests;
+
+use AvtoDev\EventsLogLaravel\Events\AbstractLoggableEvent;
+use Illuminate\Foundation\Application;
+use Illuminate\Config\Repository as ConfigRepository;
+
+/**
+ * Class FeatureTest.
+ *
+ * @coversNothing
+ */
+class FeatureTest extends AbstractTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function afterApplicationBootstrapped(Application $app)
+    {
+        /** @var ConfigRepository $config */
+        $config = $app->make('config');
+
+        $config->set('logging.channels.' . $channel_name = 'events-stack', [
+            'driver'   => 'stack',
+            'channels' => ['events-monolog', 'events-logstash'],
+        ]);
+
+        $config->set('logging.channels.events-monolog', [
+            'driver' => 'single',
+            'path'   => storage_path('logs/laravel-events.log'),
+            'level'  => 'debug',
+        ]);
+
+        $config->set('logging.channels.events-logstash', [
+            'driver' => 'custom',
+            'via'    => \AvtoDev\EventsLogLaravel\Logging\EventsLogstashLogger::class,
+            'path'   => storage_path('logs/logstash/laravel-events.log'),
+            'level'  => 'debug',
+        ]);
+
+        $config->set('logging.events_channel', $channel_name);
+
+        parent::afterApplicationBootstrapped($app);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->clearLog();
+    }
+
+    /**
+     * Feature test for a writing log message format.
+     *
+     * @return void
+     */
+    public function testWriting(): void
+    {
+        $event = new class extends AbstractLoggableEvent {
+            public function logMessage(): string
+            {
+                return 'foo message';
+            }
+        };
+
+        event($event);
+
+        $this->assertLogFileContains('"source":"UNKNOWN"', $events_log = 'laravel-events.log');
+        $this->assertLogFileContains('"type":"UNKNOWN"', $events_log);
+        $this->assertLogFileContains('"name":"class@anonymous', $events_log);
+        $this->assertLogFileContains($this->app->environment() . '.INFO: foo message', $events_log);
+
+        $this->assertLogFileContains('"entry_type":"event"', $events_logstash_log = 'logstash/laravel-events.log');
+        $this->assertLogFileContains('"name":"class@anonymous', $events_logstash_log);
+        $this->assertLogFileContains('"level":"INFO"', $events_logstash_log);
+        $this->assertLogFileContains('"event":{', $events_logstash_log);
+        $this->assertLogFileContains('"message":"foo message"', $events_logstash_log);
+    }
+}
