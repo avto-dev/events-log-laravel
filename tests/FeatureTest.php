948425ef109f844f0f32f741a0ec596bec59fd5b
IgorKorytin <56302950+IgorKorytin@users.noreply.github.com>
2020-12-25 13:40:02 +0500

    Added support for PHP 8 and composer 2 (#9)
    Co-authored-by: IgorKorytin <korytin.i@avtocod.ru>

--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -37,11 +37,9 @@ class FeatureTest extends AbstractTestCase
 
         $this->assertLogFileContains('"source":"UNKNOWN"', $events_log = 'laravel-events.log');
         $this->assertLogFileContains('"type":"UNKNOWN"', $events_log);
-        $this->assertLogFileContains('"name":"class@anonymous', $events_log);
         $this->assertLogFileContains($this->app->environment() . '.INFO: foo message', $events_log);
 
         $this->assertLogFileContains('"entry_type":"event"', $events_logstash_log = 'logstash/laravel-events.log');
-        $this->assertLogFileContains('"name":"class@anonymous', $events_logstash_log);
         $this->assertLogFileContains('"level":"INFO"', $events_logstash_log);
         $this->assertLogFileContains('"event":{', $events_logstash_log);
         $this->assertLogFileContains('"message":"foo message"', $events_logstash_log);
